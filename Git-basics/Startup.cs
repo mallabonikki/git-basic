27542012b573c3000c3fa6f55194d52430d5dd1d
mallabonikki <mallabonikki6678@gmail.com>
2017-03-05 00:54:27 +1100

    Add project files.

---
+++ Git-basics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git_basics.Startup))]
+namespace Git_basics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
